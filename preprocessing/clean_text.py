716819887ca3a6fdd0059cebce8c1e043381c81b
Milad Tavakoli <milad.tavakoli81@gmail.com>
2025-12-01 13:31:50 +0330

    Implement text cleaning function in clean_text.py

--- preprocessing/clean_text.py
+++ preprocessing/clean_text.py
@@ -1,3 +1,22 @@
+import re
+from hazm import Normalizer
+
+normalizer = Normalizer()
+
 def clean_text(text: str) -> str:
-    # TODO: implement cleaning
+    if not text:
+        return ""
+
+    #remove HTML tags
+    text = re.sub(r"<.*?>", " ", text)
+
+    #persian text normilize
+    text = normalizer.normalize(text)
+
+    #remove emojies
+    text = re.sub(r"[^\w\sآ-ی]", " ", text)
+
+    #remove extra spaces
+    text = re.sub(r"\s+", " ", text).strip()
+
     return text
