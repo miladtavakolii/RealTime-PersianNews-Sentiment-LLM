cb72ff3cd82773d38865e8d9a1c5cb0d2c3640f7
Milad Tavakoli <milad.tavakoli81@gmail.com>
2025-12-03 16:05:23 +0330

    feat: add write_last_timestamp function to save  timestamps for spiders

---
+++ scheduler/write_last_timestamp.py
@@ -0,0 +1,24 @@
+import json
+import os
+
+
+def write_last_timestamp(spider_name: str, timestamp: int, meta_dir: str = "meta") -> None:
+    """
+    Write the timestamp to the meta directory.
+
+    Args:
+        spider_name: The name of the spider for which the timestamp is being set
+        timestamp: The timestamp value to be saved
+        meta_dir: Directory to save the timestamp file
+    """
+    if not os.path.exists(meta_dir):
+        os.makedirs(meta_dir)
+
+    file_path = f"{meta_dir}/{spider_name}_last.json"
+
+    with open(file_path, "w", encoding="utf-8") as f:
+        json.dump({"last_timestamp": timestamp},
+                  f, ensure_ascii=False, indent=4)
+
+    print(
+        f"[Timestamp] Written timestamp {timestamp} for {spider_name} in {file_path}")
