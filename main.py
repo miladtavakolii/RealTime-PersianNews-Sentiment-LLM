cbb9a5cb050de50ed07e2294119762cabb1b24e6
Milad Tavakoli <milad.tavakoli81@gmail.com>
2025-12-04 18:43:12 +0330

    feat: implement ServiceRunner to manage scheduler and preprocess worker

---
+++ main.py
@@ -0,0 +1,35 @@
+from twisted.internet import asyncioreactor
+asyncioreactor.install()
+import asyncio
+from utils.config_manager import ConfigManager
+from scheduler.scrapy_scheduler import ScrapyScheduler
+from workers.preprocess_worker import PreprocessWorker
+import logging
+
+
+class ServiceRunner:
+    def __init__(self, config_path: str = "config/settings.yaml"):
+        config = ConfigManager(config_path)
+        self.scheduler = ScrapyScheduler(config.get_spider_configs())
+        self.preprocess_worker = PreprocessWorker()
+
+    async def start_Preprocess_worker(self):
+        print("[Main] Starting PreprocessWorker...")
+        await asyncio.to_thread(self.preprocess_worker.start)
+
+    async def run(self):
+        # Run both scheduler and worker concurrently
+        await asyncio.gather(
+            self.start_Preprocess_worker(),
+            self.scheduler.start(),
+        )
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    logging.getLogger("pika").setLevel(logging.WARNING)
+    logging.getLogger("pika").disabled = True
+
+
+    runner = ServiceRunner()
+    asyncio.run(runner.run())
